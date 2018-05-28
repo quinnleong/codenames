7720a5df986e2af9d1fbc487859dcebbee089eb5
Jennifer Georgevich <jenngeorge@berkeley.edu>
2018-05-27 23:50:37 -0400

    Add script and styles files

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("HIIII")
+
+    const words = ['word0', 'word1', 'word2']
+
+    const board = document.getElementById("div1")
+    words.forEach(function(word) {
+      board.append
+      // append a node 
+    })
+  })
